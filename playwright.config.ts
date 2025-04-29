f5b70229fcdcca4ebc90621f39e8e8f8a87181c4
Grzeg Krol <grzegorz.krol@vimn.com>
2025-04-29 13:56:36 +0200

    refactor: rename test to e2e env

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, devices } from "@playwright/test";
 import dotenv from "dotenv";
 import path from "path";
 
-dotenv.config({ path: path.resolve(process.cwd(), ".env.test") });
+dotenv.config({ path: path.resolve(process.cwd(), ".env.e2e") });
 
 export default defineConfig({
   testDir: "./e2e",
